3c116cff4f625d55323526c851dcdee31e858434
windschief <windschief3@gmail.com>
2024-12-24 15:59:57 +0100

    Add json ignore to album item

--- src/main/java/com/windschief/spotify/model/AlbumItem.java
+++ src/main/java/com/windschief/spotify/model/AlbumItem.java
@@ -2,20 +2,23 @@ package com.windschief.spotify.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record AlbumItem(
-        String album_type,
-        int total_tracks,
-        List<String> available_markets,
-        ExternalUrls external_urls,
-        String href,
-        String id,
-        List<Image> images,
-        String name,
-        String release_date,
-        String release_date_precision,
-        Restrictions restrictions,
-        String type,
-        String uri,
-        List<ArtistItem> artists,
-        String album_group
-) {}
+                String album_type,
+                int total_tracks,
+                List<String> available_markets,
+                ExternalUrls external_urls,
+                String href,
+                String id,
+                List<Image> images,
+                String name,
+                String release_date,
+                String release_date_precision,
+                Restrictions restrictions,
+                String type,
+                String uri,
+                List<ArtistItem> artists,
+                String album_group) {
+}
