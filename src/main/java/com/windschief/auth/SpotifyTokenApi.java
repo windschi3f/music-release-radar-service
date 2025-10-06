c10effa8359e09e657b945bd502cf33c6ec7036b
vinci <windschief3@gmail.com>
2025-10-06 22:43:26 +0200

    Add tag for spotify auth api

--- src/main/java/com/windschief/auth/SpotifyTokenApi.java
+++ src/main/java/com/windschief/auth/SpotifyTokenApi.java
@@ -3,6 +3,8 @@ package com.windschief.auth;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
 import io.quarkus.security.Authenticated;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.PUT;
@@ -11,6 +13,7 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/auth/spotify")
+@Tag(name = "Spotify Authentication", description = "Operations for managing Spotify authentication")
 public interface SpotifyTokenApi {
 
     @PUT
