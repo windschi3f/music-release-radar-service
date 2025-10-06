b2c144b7fa51ebcdcc9e590b0ab5fb4a1077f4d2
vinci <windschief3@gmail.com>
2025-10-06 22:41:36 +0200

    Add openAPI annotation for added items

--- src/main/java/com/windschief/task/added_item/AddedItemApi.java
+++ src/main/java/com/windschief/task/added_item/AddedItemApi.java
@@ -1,5 +1,10 @@
 package com.windschief.task.added_item;
 
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -11,7 +16,15 @@ import jakarta.ws.rs.core.Response;
 @Path("/tasks/{taskId}/added-items")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@Tag(name = "Added Item Management", description = "Operations for managing added items")
 public interface AddedItemApi {
     @GET
+    @APIResponse(
+        responseCode = "200",
+        content = @Content(
+            mediaType = MediaType.APPLICATION_JSON,
+            schema = @Schema(implementation = AddedItem.class)
+        )
+    )
     Response getAddedItems(@PathParam("taskId") Long taskId);
 }
